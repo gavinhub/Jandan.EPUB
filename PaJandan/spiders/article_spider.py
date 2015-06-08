3dacdd1bd55db4210dc232edd7dc83e6b1a5328a
GavinCode <gmy.gavin@gmail.com>
2015-06-08 11:14:21 +0800

    Set default month to .

--- PaJandan/spiders/article_spider.py
+++ PaJandan/spiders/article_spider.py
@@ -18,22 +18,23 @@ class ArtSpider(CrawlSpider):
             Rule(LinkExtractor(allow=['/([0-9]+/){3}.+html']), callback='parse_art', follow=False),
         ]
 
-    def __init__(self, month="2015/05", *args, **kwargs):
+    def __init__(self, month=None, *args, **kwargs):
         super(ArtSpider, self).__init__(*args, **kwargs)
         ArtSpider.start_urls = []
-        if month:
-            date = month.split('/')
-            Y, m = int(date[0]), int(date[1])
-            days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-            dayrange = range(1, days[m]+1)
-            if m == 2 and (Y%400==0 or (Y%4==0 and Y%100!=0)):
-                dayrange.append(29)
-            digit = lambda s: str(s) if len(str(s))==2 else '0'+str(s)
-            for d in dayrange:
-                datestr = str(Y) + '/' + digit(m) + '/' + digit(d)
-                ArtSpider.start_urls.append("http://jandan.net/%s" % datestr)
-        else:
-            ArtSpider.start_urls.append("http://jandan.net/")
+        # If month not indecated, set to `this month` 
+        if not month:
+           month = Time.strftime("%Y/%m")
+        # Traversal the days
+        date = month.split('/')
+        Y, m = int(date[0]), int(date[1])
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        dayrange = range(1, days[m]+1)
+        if m == 2 and (Y%400==0 or (Y%4==0 and Y%100!=0)):
+            dayrange.append(29)
+        digit = lambda s: str(s) if len(str(s))==2 else '0'+str(s)
+        for d in dayrange:
+            datestr = str(Y) + '/' + digit(m) + '/' + digit(d)
+            ArtSpider.start_urls.append("http://jandan.net/%s" % datestr)
         
     
     def parse_art(self, response):
@@ -51,7 +52,7 @@ class ArtSpider(CrawlSpider):
         # content
         content = response.xpath("//div[@class='post f'][1]/*")[4:-4]
         l.add_value("content", content.extract())
-        # images
+        # images: put urls into image_urls[], then pipline will download them.
         imgs = content.xpath("img/@src")
         l.add_value("image_urls", imgs.extract())
         return l.load_item()
